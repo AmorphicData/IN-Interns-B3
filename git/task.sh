ff02d8658c163c2e19bd56023786da118146afc7
Harshini <harshini.bollineni@cloudwick.com>
2024-02-14 09:54:08 +0530

    Modified bash file

--- git/task.sh
+++ git/task.sh
@@ -1,81 +1,56 @@
 #!/bin/bash
-#Author Name: Harshini
-#Publish Date: May30 , 2023
-#Last Updated: Feb 13, 2024
-#Master branch name hardcoded.
-main=“main”
+# Author Name: Harshini
+# Publish Date: Feb 11, 2024
+# Last Updated: Feb 14, 2024
+# Master branch name hardcoded.
+main="main"
 git checkout $main
 git pull origin $main
-prefix=“origin/”
-file=“$1”
-if [ -s “$file” ];
-then
-	echo “List of branches:”
-	while read -r line;
-  	do
-    		echo “$line”
-	done < “$file”
+prefix="origin/"
+file="$2"
+if [ -s "$file" ]; then
+  echo "List of branches:"
+  while read -r line; do
+    echo "$line"
+  done < "$file"
 else
-	git branch -r
+  git branch -r
 fi
-#Creation of a array for valid branches only!
+# Creation of an array for valid branches only
 branches_array=()
-echo “Validating from remote branches....”
-for i in $(git branch -r);
-do
-	branch=“${i#$prefix}”
-	if grep -q -e “$branch” “$file”;
-        then
-		echo “Branch $branch found in remote”
-		branches_array+=(“$branch”)
-	fi
+echo "Validating from remote branches"
+for i in $(git branch -r); do
+  branch="${i#$prefix}"
+  if grep -q -e "$branch" "$file"; then
+    echo "Branch $branch found in remote"
+    branches_array+=("$branch")
+  fi
 done
-read -p “Check for updates?(yes/no):” flag
-case $flag in
-	“yes”)	
-		echo “Checking for updates in valid branches....”
-        	for branch in “${branches_array[@]}“;
-        	do
-			echo “Checking branch: $branch”
-            		git checkout “$branch” 2>/dev/null
-      			upstream=$(git rev-parse --abbrev-ref --symbolic-full-name “$branch@{upstream}” 2>/dev/null)
-      			if [ -n “$upstream” ]; then
-        			if git merge-base --is-ancestor “$upstream” “$branch”; then
-					echo “=========================”
-        			else
-					echo “=========================”
-        			fi
-      			else
-        			echo “The branch ‘$branch’ doesn’t have any commits ahead.”
-      			fi
-    		done
-	;;
-	“no”)
-		echo “Exiting now.”
-		exit
-	;;
-	*)
-		echo “Wrong input. Exiting!”
-	;;
-esac
-read -p “Want to continue merge into main(yes/no):” chk
+if [${#branches_array[@]} -eq 0];
+ then
+ echo"Array is empty"
+else
+ echo "Array is not empty"
+fi
+read -p "Want to continue merge into main(yes/no): " chk
 case $chk in
-	“yes”)
-		for branch in “${branches_array[@]}“;
-		do
-  			echo “Merging branch $branch”
-			git checkout $main
-			git merge -X theirs -m “Merging branch $branch in main” $branch
-   			sleep 5
-		done
-		;;
-	“no”)
-		echo “Exiting!”
-		exit
-		;;
-	*)
-		echo “Wrong Input! Exiting!”
-		exit
-		;;
+  "yes")
+    for branch in "${branches_array[@]}"; do
+      echo "Merging main in branch $branch"
+      git checkout $branch
+      git pull origin $branch
+      git merge -X ours -m "Merging branch $branch into $main" $branch
+      git push origin "$branch"
+      sleep 5
+    done
+    ;;
+  "no")
+    echo "Exiting!"
+    exit
+    ;;
+  *)
+    echo "Wrong Input! Exiting!"
+    exit
+    ;;
 esac
 
