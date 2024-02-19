2aa218e2a6db443dcadca57b58ba9f2419cb2f7e
Saksham Dhillon <saksham.dhillon@cloudwick.com>
2024-02-19 10:44:31 +0530

    Updated gittask3

--- git/gittask3.sh
+++ git/gittask3.sh
@@ -1,32 +1,31 @@
-#!/bin/bash
-#Author Name:Saksham Dhillon
-#Publish Date: 14 feb 2024
-#Last Updated: 18 feb 2024
-exec < /dev/tty ## helps in forcing the script to take keyboard input
-commit_msg_file="$1"
-commit_msg=$(cat "$commit_msg_file")
-allowed_prefixes=("add" "update" "remove" "refactor" "bug-fix" "document" "updated" "Added" "Test" "bug-Fix")
-# Get the commit message
-# echo $commit_msg
-# Validate the prefix from the list of allowed prefixes
-valid_prefix=false
+# #!/bin/bash
+# ## Pre-commit hook script for checking commit message
+# # Author Name: Saksham Dhillon
+# # Publish Date: Feb 14, 2024
+# # Last Updated: Feb 19, 2024
+exec < /dev/tty
+commit_message_file="$1"
+commit_message=$(cat "$commit_message_file")
+allowed_prefixes=("add" "update" "remove" "refactor" "bug-fix" "document")
+prefix_valid=false
 for prefix in "${allowed_prefixes[@]}"; do
-    if [[ "$commit_msg" =~ ^"$prefix":.+ ]]; then
-        valid_prefix=true
+    if [[ "$commit_message" == "${prefix}"* ]]; then
+        prefix_valid=true
         break
     fi
 done
-#echo $valid_prefix
-# Exit with a warning if validation fails
-if [ "$valid_prefix" = false ]; then
-    echo "Error: Commit message prefix is not one of the allowed prefixes:"
-    printf '%s:\n' "${allowed_prefixes[@]}"
+if [ "$prefix_valid" != true ]; then
+    echo "Error: Invalid commit message. Allowed prefixes are: ${allowed_prefixes[@]}"
     exit 1
 fi
-# Prompt user for confirmation to continue the commit
-echo "Commit message: $commit_msg"
-read -p "Do you want to continue with this commit? (yes/no): " answer
-if [ "$answer" != "yes" ]; then
-    echo "Commit aborted."
-    exit 1
-fi
+echo "Commit message: $commit_message"
+read -p "Do you want to continue with the commit? (yes/no): " choice
+case "$choice" in
+    yes)
+        exit 0
+        ;;
+    *)
+        echo "Commit aborted by user."
+        exit 1
+        ;;
+esac
