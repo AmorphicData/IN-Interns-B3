97dc53e62696543fedf441b32faaacf4c9f32b95
Nayan Sachan <nayan.sachan@gmail.com>
2024-03-04 11:54:26 +0530

    Submitted AWS Assignment

---
+++ aws/aws_cli_cloudshell/assignment1.sh
@@ -0,0 +1,26 @@
+#!bin/bash
+
+if !aws --version &> /dev/null;then
+    echo "AWS CLI is not installed. Please Install it Before Running"
+    exit 1
+fi
+
+echo "AWS CLI version:"
+aws --version
+
+read -p “Enter your name:" user
+
+if ! aws configure get profile.$user &> /dev/null; then
+    echo "Error: AWS profile with the name is not set."
+    exit 1
+fi
+
+echo "IAM Roles:"
+aws iam list-roles --profile $user | jq -r '.Roles[].RoleName'
+
+echo "IAM Roles starting with 'AWS':"
+aws iam list-roles --profile $user | jq -r '.Roles[].RoleName' | grep '^AWS'
+
+# S3 bucket names
+echo "S3 Bucket names:"
+aws s3api list-buckets --profile $user | jq -r '.Buckets[].Name'
