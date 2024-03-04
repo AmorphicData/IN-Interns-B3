cb5e0667b999338e837cfcaa15a81f844502bcd9
Saksham Dhillon <saksham.dhillon@cloudwick.com>
2024-03-04 09:46:27 +0530

    Created aws ament.sh file

---
+++ aws/aws_cli_cloudshell/ament1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if ! aws --version &> /dev/null; then
+    echo "AWS CLI is not installed. Please install it before running this script."
+    exit 1
+fi
+aws --version
+read -p "Your Name:" name
+
+check=0
+#eq is for integer comparisons and double quotes are used if string contain any special characters or spaces
+for profile in $(aws configure list-profiles --output text --query 'Profiles');
+do
+    if [ "$profile" == "$name" ];
+     then
+     $check=1
+    fi
+done
+echo $check
+echo "All IAM Roles:"                                   #List out all IAM roles
+aws iam list-roles --profile $name --query "Roles[].RoleName" --output text
+echo "IAM Roles starting with 'AWS':"                      #List of all IAM Roles starting with 'AWS'
+aws iam list-roles --profile $name --query "Roles[?starts_with(RoleName, 'AWS')].RoleName" --output text
+
+aws s3api list-buckets --profile $profile_name --query "Buckets[*].Name" --output text
+
+
